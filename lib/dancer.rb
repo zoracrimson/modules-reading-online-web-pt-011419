718d1d2cf2031ca76cb09dafd869cd73718db819
Kat Maldon <katharinamaldon@gmail.com>
2019-02-21 20:13:59 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer 
+  extend MetaDancing
+  include Dance 
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+end 
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid 
+  extend MetaDancing
+  include Dance 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end 
+end 
